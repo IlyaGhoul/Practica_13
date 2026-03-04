192d2ef375b8577afdcc470b32329ea1ad33c7da
Ilya <laptevila186@gmail.com>
2026-03-04 14:16:48 +0500

    Update

--- 7.py
+++ 7.py
@@ -1,8 +1,33 @@
 import sqlite3
 
 connection = sqlite3.connect('my_database.db')
-
 cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users (
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER,
+    is_active INTEGER DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+''')
+
+cursor.execute('CREATE INDEX IF NOT EXISTS idx_username ON Users (username)')
+
+cursor.execute('''
+CREATE VIEW IF NOT EXISTS ActiveUsers AS
+SELECT * FROM Users WHERE is_active = 1
+''')
+
+cursor.execute('''
+CREATE TRIGGER IF NOT EXISTS update_created_at
+AFTER INSERT ON Users
+BEGIN
+    UPDATE Users SET created_at = CURRENT_TIMESTAMP WHERE id = NEW.id;
+END;
+''')
 
 query = 'SELECT * FROM Users WHERE age > ?'
 cursor.execute(query, (25,))
@@ -11,34 +36,11 @@ users = cursor.fetchall()
 for user in users:
     print(user)
 
-cursor.execute('CREATE VIEW ActiveUsers AS SELECT * FROM Users WHERE is_active = 1')
-
 cursor.execute('SELECT * FROM ActiveUsers')
 active_users = cursor.fetchall()
 
 for user in active_users:
     print(user)
 
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS Users (
-               id INTEGER PRIMARY KEY,
-               username TEXT NOT NULL,
-               email TEXT NOT NULL,
-               age INTEGER,
-               created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
-               ''')
-
-cursor.execute('''
-CREATE TRIGGER IF NOT EXISTS update_created_at
-               AFTER INSERT Users
-               BEGIN
-               UPDATE Users SET created_at = CURRENT_TIMESTAMP WHERE
-               id = NEW.id;
-               END;
-               ''')
-
-cursor.execute('CREATE INDEX idx_username ON Users (username)')
-
 connection.commit()
 connection.close()
-
